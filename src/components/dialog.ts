524920862d988f3ae310c46748c2b315bef0ebd1
Charlie Draper <charles.draper@brandwatch.com>
2023-01-09 16:40:44 +0000

    feat<tooltip>: add floating ui

--- src/components/dialog.ts
+++ src/components/dialog.ts
@@ -1,6 +1,6 @@
-import { LitElement, css, html, PropertyValues } from 'lit';
-import { customElement, property, query, state } from 'lit/decorators.js';
-import './tooltip';
+import { LitElement, css, html, PropertyValues } from "lit";
+import { customElement, property, query, state } from "lit/decorators.js";
+import "./tooltip";
 
 /**
  * A Dialog Element.
@@ -8,28 +8,20 @@ import './tooltip';
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('ds-dialog')
+@customElement("ds-dialog")
 export class Dialog extends LitElement {
   @state()
   isOpen: boolean = false;
 
-  @query('#dialog')
+  @query("#dialog")
   dialog: HTMLDialogElement;
 
   static styles = css`
     .dialog__content {
-      display: flex;
-    }
-    .cta {
-      margin: 10em;
     }
 
-    :host{
-      margin: auto;
-    }
-
-    #btn{
-      font-size: 20px;
+    .dialog {
+      overflow: hidden;
     }
   `;
 
@@ -40,41 +32,37 @@ export class Dialog extends LitElement {
 
   render() {
     return html`
-    <div>
-      <button @click=${this.toggleDialog} id="btn">
-        <slot name="title">Click me</slot>
-      </button>
-      <dialog id="dialog">
-        <slot name="content" class="dialog__content">
-          <p>This is my dialog</p>
+      <div>
+        <button @click=${this.toggleDialog} id="btn">
+          <slot name="title">Click me</slot>
+        </button>
+        <dialog class="dialog" id="dialog">
+          <slot name="content" class="dialog__content">
+            <p>This is my dialog</p>
 
-          <!-- tooltip -->
-          <ds-tooltip>
-            <p>tooltip 1</p>
+            <!-- tooltip -->
             <ds-tooltip>
-              <p>tooltip 2</p>
-              <p>woah</p>
+              <p>tooltip 1</p>
+              <ds-tooltip>
+                <p>tooltip 2</p>
+              </ds-tooltip>
             </ds-tooltip>
-          </ds-tooltip>
-        </slot>
+          </slot>
 
-        <!-- close button -->
-        <slot name="footer" class="cta">
-          <form method="dialog">
-            <button>OK</button>
-          </form>
-        </slot>
-      </dialog>
-    </div>
+          <!-- close button -->
+          <slot name="footer" class="cta">
+            <form method="dialog">
+              <button>OK</button>
+            </form>
+          </slot>
+        </dialog>
+      </div>
     `;
   }
-
-  static styles = css`
-  `;
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'ds-dialog': Dialog;
+    "ds-dialog": Dialog;
   }
 }
--- src/components/tooltip.ts
+++ src/components/tooltip.ts
@@ -1,59 +1,87 @@
-import { css, html, LitElement } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
-import { styleMap } from 'lit/directives/style-map.js';
+import {
+  computePosition,
+  flip
+} from "@floating-ui/dom";
+import { css, html, LitElement } from "lit";
+import { customElement, property, query, state } from "lit/decorators.js";
+import { styleMap } from "lit/directives/style-map.js";
 
-@customElement('ds-tooltip')
+@customElement("ds-tooltip")
 export class Tooltip extends LitElement {
   @state()
   isHovering: boolean;
 
   @property()
-  positioning: 'absolute' | 'fixed' = 'absolute';
+  positioning: "absolute" | "fixed" = "absolute";
+
+  @query(".invoker")
+  invoker: HTMLElement;
+
+  @query(".content")
+  content: HTMLElement;
 
   static styles = css`
     .invoker {
       width: 20px;
       height: 20px;
       border: 1px solid red;
+      position: relative;
     }
 
     .content {
       /* use position fixed to paint over an overflow hidden parent */
       /* position: fixed; */
-      padding: 2em;
+      margin: 0;
+      padding: 5px;
+      position: absolute;
     }
   `;
 
   enterHandler() {
-    console.log('is entering');
+    console.log("is entering", this.invoker.style);
+    this.computeTooltipPosition();
     this.isHovering = true;
   }
 
   leaveHandler() {
-    console.log('is leaving');
+    console.log("is leaving");
     this.isHovering = false;
+  }
+
+  computeTooltipPosition() {
+    const { invoker, content } = this;
+    computePosition(invoker, content, {
+      placement: "right",
+      middleware: [flip()],
+    }).then(({ x, y }) => {
+      Object.assign(content.style, {
+        left: `${x}px`,
+        top: `${y}px`,
+      });
+    });
   }
 
   render() {
     const displayTooltip = {
-      opacity: this.isHovering ? '1' : '0',
+      visibility: this.isHovering ? "visible" : "hidden",
       position: this.positioning,
     };
     return html`
-      <div class='invoker' @mouseenter=${this.enterHandler} @mouseleave=${
-      this.leaveHandler
-    }>
-      
+      <div
+        class="invoker"
+        @mouseenter=${this.enterHandler}
+        @mouseleave=${this.leaveHandler}
+      >
         <dialog class="content" open style=${styleMap(displayTooltip)}>
           <slot></slot>
         </dialog>
       </div>
-      `;
+    `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'ds-tooltip': Tooltip;
+    "ds-tooltip": Tooltip;
   }
 }
